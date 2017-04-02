09eb026bbbe26918037c5a0b9270ee46966dfa9c
Vu Hai Ninh <haininh.vu89@gmail.com>
2017-04-02 20:07:36 +0300

    update example style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,23 @@
 var React = require('react');
-
+var {Link} = require('react-router');
 var Examples = React.createClass({
   render: function(){
     return(
-      <h3>Examples Component!!!</h3>
+      <div>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are few example location to try out</p>
+        <ol>
+          <li>
+            <Link to='/?location=Philadelphia'>Philadelphia, PA</Link>
+          </li>
+          <li>
+            <Link to= "/?location=Miami"> Miami</Link>
+          </li>
+        </ol>
+
+      </div>
+
+
     );
   }
 });
